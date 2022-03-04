acd6ba51462440d1bbf1b6c45aea56dbdcd3ef4c
Syed Mazhar Ali Raza <mazharali.raza11@gmail.com>
2022-03-04 13:38:00 +0530

    added user auth in backend

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import {
   notFoundHandler,
   errorHandler,
@@ -17,6 +18,8 @@ connectDB();
 const app = express();
 
 // Middlewares
+app.use(express.json());
+app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/products/:id", productRoutes);
 app.use(notFoundHandler);
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,11 @@
 import express from "express";
-import Product from "../models/productModel.js";
-import expressAsyncHandler from "express-async-handler";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
-
-router.get(
-  "/",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-router.get(
-  "/:id",
-  expressAsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
 
 export default router;
