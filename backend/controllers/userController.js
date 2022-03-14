0024e32cce3fa448b41658bb58a39eefe5f74594
Syed Mazhar Ali Raza <mazharali.raza11@gmail.com>
2022-03-14 18:32:26 +0530

    user login/logout added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
+import bcrypt from "bcryptjs";
 
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -17,6 +18,35 @@ export const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401);
     throw new Error("Invalid email or password");
+  }
+});
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(404);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, 10),
+  });
+
+  if (user) {
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
   }
 });
 
---
+++ frontend/src/views/LoginPage.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { Link, useSearchParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import { login } from "../actions/userActions";
+import Loader from "../components/ui/Loader";
+import Message from "../components/ui/Message";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+
+  const { loading, error, userInfo } = userLogin;
+
+  const redirect = searchParams.get("redirect")
+    ? searchParams.get("redirect")
+    : "";
+
+  useEffect(() => {
+    console.log(userInfo);
+    if (userInfo) {
+      return navigate(`/${redirect}`);
+    }
+  }, [userInfo, redirect, navigate]);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch(login(email, password));
+  }
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      {loading && <Loader />}
+      {error && <Message variant='danger'>{error}</Message>}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email' className='my-3'>
+          <Form.Label style={{ fontWeight: 500 }}>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label style={{ fontWeight: 500 }}>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary' className='my-3'>
+          Sign In
+        </Button>
+      </Form>
+      <Row className='py-3'>
+        <Col>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : `/register`}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginPage;
